2bc5286f2a2c10f5f8f93e7532fbe987994ef73d
dstefani <don@fiftyflowers.com>
2016-02-17 11:49:06 -0700

    added more file comments and deleted an unused layout file

--- includes/MyPage/View/layouts/customer_nav.php
+++ includes/MyPage/View/layouts/customer_nav.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * This is the layout for the data view file: templates/_cust_nav_elems.php
+ * You can call the data from the template file simply with the getContent() method
+ * and pop that into some plain HTML
+ */
+
+?>
+
 <nav>
 <ul class="navlist">
   <?= $this->getContent() ?>
--- includes/MyPage/View/templates/_cust_nav_elems.php
+++ includes/MyPage/View/templates/_cust_nav_elems.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * This is the data portion of a two step view
+ * the companion layout file is layouts/customer_nav.php
+ */
 foreach ($this->cust_fnames as $cust) {
   $fname = $this->escape()->html($cust['first_name']);
   echo "<li><a href=\"#\">{$fname}</a></li>\n";
